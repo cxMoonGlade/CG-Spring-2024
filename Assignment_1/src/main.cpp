351b96b2e7014b1c4601b466c7a9a467682cae90
cxMoonGlade <920977575@qq.com>
2024-02-07 16:03:17 -0800

    assignment#1

--- Assignment_1/src/main.cpp
+++ Assignment_1/src/main.cpp
@@ -4,6 +4,8 @@
 #include <fstream>
 #include <iostream>
 #include <numeric>
+#include <stdexcept>
+#include <string>
 #include <vector>
 
 #include <Eigen/Dense>
@@ -16,15 +18,25 @@ const std::string root_path = DATA_DIR;
 // Computes the determinant of the matrix whose columns are the vector u and v
 double inline det(const Vector2d &u, const Vector2d &v)
 {
-    // TODO
-    return 0;
+    return u.x() * v.y() - u.y() * v.x();
 }
 
 // Return true iff [a,b] intersects [c,d]
 bool intersect_segment(const Vector2d &a, const Vector2d &b, const Vector2d &c, const Vector2d &d)
 {
-    // TODO
-    return true;
+    double area_change = det(b - a, d - c);
+    if (area_change == 0) return false;
+
+    Vector2d r = c - a, res;
+
+    double t = det(r, d - c) / area_change;
+    double u = det(r, b - a) / area_change;
+
+    if (t >= 0 && t <= 1 && u >= 0 && u <= 1){
+        res = a + t * (b - a);
+        return true;
+    }
+    return false;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -33,10 +45,14 @@ bool is_inside(const std::vector<Vector2d> &poly, const Vector2d &query)
 {
     // 1. Compute bounding box and set coordinate of a point outside the polygon
     // TODO
-    Vector2d outside(0, 0);
-    // 2. Cast a ray from the query point to the 'outside' point, count number of intersections
-    // TODO
-    return true;
+    Vector2d edge_point (1e18, query.y());
+    int cnt = 0;
+    size_t n = poly.size();
+    
+    for (size_t i = 0; i < n; i++){
+        if (intersect_segment(poly[i], poly[(i + 1) % n], query, edge_point)) ++ cnt;
+    }
+    return cnt %2 == 1;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -45,13 +61,57 @@ std::vector<Vector2d> load_xyz(const std::string &filename)
 {
     std::vector<Vector2d> points;
     std::ifstream in(filename);
-    // TODO
+    if (!in.is_open()) {
+        throw std::runtime_error("Failed to open file: " + filename);
+    }
+
+    std::string line;
+    int point_cnt = 0;
+
+    if (std::getline(in, line)){
+        try{
+            point_cnt = std::stoi(line);
+        }catch(const std::invalid_argument &e){
+            throw std::runtime_error("Invalid Point Count: " + line);
+        }catch (const std::out_of_range &e){
+            throw std::runtime_error("Point Count Out of Range" + line);
+        }
+        points.reserve(point_cnt);
+        for (int i = 0; i < point_cnt; i ++){
+            if (std::getline(in, line)){
+                std::istringstream iss(line);
+                double x = 0.0, y = 0.0;
+                
+                if (!(iss >> x >> y)){
+                    throw std::runtime_error("Failed to Read Coordinates at Line: " + line); 
+                }
+                points.emplace_back(x, y);
+            } else {
+                throw std::runtime_error("Unexpected End of File While Reading Points.");
+            }
+        }
+    } else {
+        throw std::runtime_error("Failed to Read Point Count");
+    }
+    in.close();
     return points;
 }
 
 void save_xyz(const std::string &filename, const std::vector<Vector2d> &points)
 {
-    // TODO
+    std::ofstream outs (filename);
+    
+    if (!outs.is_open()){
+        throw std::runtime_error("Failed to Open File:" + filename);
+    }
+
+    outs << points.size() << "\n";
+    
+    for (const auto &p : points){
+        outs << p.x() << ' ' << p.y() << "0\n";
+    }
+
+    outs.close();
 }
 
 std::vector<Vector2d> load_obj(const std::string &filename)
